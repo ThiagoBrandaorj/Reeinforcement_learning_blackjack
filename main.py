a6905a622a5d37722bcad597e227bc7f3407c076
Thiago <thiagocbrandao2003@gmail.com>
2025-10-20 17:43:17 -0300

    jogando 1000 jogos com política fixa

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import gymnasium as gym
 env = gym.make('Blackjack-v1', render_mode='human')
-observation, info = env.reset()
-done = False
-while not done:
-    env.render()
-    observation, reward, done, truncated, info = env.step(action)
-    state = observation
-    player_sum,dealer_card,usable_ace = state
-    if player_sum >= 17:
-        action = 0
-    else:
-        action = 1
-print(f'Reward final: {reward}')
-print(done)
-print(truncated)
-print(info)
-print(observation)
+results = []
+num_games = 1000
+for game in range(num_games):
+    i = 0
+    done = False
+    print(f'Iniciando o jogo {game}')
+    observation, info = env.reset()
+    while not done:
+        env.render()
+        state = observation
+        player_sum,dealer_card,usable_ace = state
+        print(f'Estado {i} -> Player sum: {player_sum}, Dealer card: {dealer_card}, Usable ace: {usable_ace}')
+        if player_sum >= 17:
+            action = 0
+        else:
+            action = 1
+        print(f'Ação tomada: {"Stick" if action == 0 else "Hit"}')
+        i += 1
+        observation, reward, done, truncated, info = env.step(action)
+        if done:
+            results.append(reward)
+        else:
+            continue
+        print(f'Recompa final do episódio: {reward}')
+        print(f'observation final do episódio: {observation}')
 env.close()
+print(len(results))
